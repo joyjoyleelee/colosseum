86e0e66e0e1f72f8c0af7c055775eea66afe5f68
Thierry <thierry.tj007@gmail.com>
2023-10-22 18:44:06 -0400

    added the likes to the database, displays static 0

--- Project 2/static/script.js
+++ Project 2/static/script.js
@@ -18,15 +18,18 @@ function deleteMessage(messageId) {
             console.log(this.response);
         }
     }
+    console.log(messageID)
     request.open("DELETE", "/chat-message/" + messageId);
     request.send();
 }
 
 function chatMessageHTML(messageJSON) {
+    console.log(messageJSON)
     const username = messageJSON.username;
     const message = messageJSON.message;
     const messageId = messageJSON.id;
     const messageTitle = messageJSON.title;
+    const likes = messageJSON.likes; // Turns the number of likes from message.JSON to number of likes
     // Line below creates the HTML post in article format.
     let messageHTML = "" +
         "<article id = 'message_" + messageId +"'>" +
@@ -34,7 +37,7 @@ function chatMessageHTML(messageJSON) {
         "<h3>" + username + "</h3>" +
         "<p>" + message + "</p>" +
         "<p><button onclick='deleteMessage(\"" + messageId + "\")' id='heart'>❤️</button></p>" +
-        "CHangeMe" +
+        "<p>" + likes + ' likes' + "</p>" +
         "</article>";
     // let messageHTML = "<br><button onclick='deleteMessage(\"" + messageId + "\")'>X</button> ";
     //messageHTML += "<span id='message_" + messageId + "'>" + "<b>" + username + "</b>: " + message + "</span>";
@@ -69,7 +72,7 @@ function sendChat() {
 
     //Insert the xsrf token in the chat (hidden) by replacing its value in the HTML
     const xsrf = document.getElementById("xsrf");
-    const messageJSON = {"title": title, "message": message, "xsrf": xsrf.value};
+    const messageJSON = {"title": title, "message": message, "xsrf": xsrf.value };
     request.open("POST", "/chat-message");
     request.send(JSON.stringify(messageJSON));
     chatTextBox.focus();
