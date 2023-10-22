d42df6397cb12cf88a2efebd2fa92cf823beafe0
marcomuy <marcomuy@buffalo.edu>
2023-10-22 04:04:13 -0400

    Core function works

--- Project 2/static/script.js
+++ Project 2/static/script.js
@@ -26,8 +26,17 @@ function chatMessageHTML(messageJSON) {
     const username = messageJSON.username;
     const message = messageJSON.message;
     const messageId = messageJSON.id;
-    let messageHTML = "<br><button onclick='deleteMessage(\"" + messageId + "\")'>X</button> ";
-    messageHTML += "<span id='message_" + messageId + "'><b>" + username + "</b>: " + message + "</span>";
+    const messageTitle = messageJSON.title;
+    // Line below creates the HTML post in article format.
+    let messageHTML = "" +
+        "<article id = 'message_" + messageId +"'>" +
+        "<h1>" + messageTitle + "</h1>" +
+        "<h3>" + username + "</h3>" +
+        "<p>" + message + "</p>" +
+        "<p><button onclick='deleteMessage(\"" + messageId + "\")'>💓</button></p>" +
+    "</article>";
+    // let messageHTML = "<br><button onclick='deleteMessage(\"" + messageId + "\")'>X</button> ";
+    //messageHTML += "<span id='message_" + messageId + "'>" + "<b>" + username + "</b>: " + message + "</span>";
     return messageHTML;
 }
 
@@ -44,8 +53,11 @@ function addMessageToChat(messageJSON) {
 }
 
 function sendChat() {
+    const chatTitle = document.getElementById("title-box");
     const chatTextBox = document.getElementById("chat-text-box");
+    const title = chatTitle.value;
     const message = chatTextBox.value;
+    chatTitle.value = "";
     chatTextBox.value = "";
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -56,8 +68,7 @@ function sendChat() {
 
     //Insert the xsrf token in the chat (hidden) by replacing its value in the HTML
     const xsrf = document.getElementById("xsrf");
-
-    const messageJSON = {"message": message, "xsrf": xsrf.value};
+    const messageJSON = {"title": title, "message": message, "xsrf": xsrf.value};
     request.open("POST", "/chat-message");
     request.send(JSON.stringify(messageJSON));
     chatTextBox.focus();
@@ -88,6 +99,7 @@ function welcome() {
     console.log("got into the welcome page")
     document.getElementById("paragraph").innerHTML += "<br/>This text was added by JavaScript 😀";
     document.getElementById("chat-text-box").focus();
+    document.getElementById("title-box").focus()
 
     updateChat();
     setInterval(updateChat, 2000);
